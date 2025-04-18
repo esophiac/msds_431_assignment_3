33e7f32a6fb034703a86d17b86d84e7147c252d0
Elizabeth Conwell <esophiac@gmail.com>
2025-04-17 20:44:55 -0400

    create methods for writing to jsonl

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"encoding/csv"
 	"encoding/json"
+
 	"fmt"
 	"log"
 	"os"
@@ -10,24 +11,10 @@ import (
 	"strings"
 )
 
-// checks to make sure that the user is providing an input file
-// and an output file
-func checkInput() {
-	if len(os.Args) == 1 {
-		fmt.Println("Please include an input file and output file name.")
-		os.Exit(0)
-	} else if len(os.Args) == 2 {
-		fmt.Println("Please include an output file name.")
-		os.Exit(0)
-	} else if len(os.Args) > 3 {
-		fmt.Println("Too many arguments.")
-		os.Exit(0)
-	}
-}
-
-// read a CSV file and return the records
-func readCsvFile(filePath string) (returnMap []map[string]int) {
+// open the file and read the results
+func createData(filePath string) [][]string {
 	f, err := os.Open(filePath)
+
 	// error handling
 	if err != nil {
 		log.Fatal("Unable to read input file "+filePath, err)
@@ -40,46 +27,95 @@ func readCsvFile(filePath string) (returnMap []map[string]int) {
 		log.Fatal("Unable to parse file as CSV for "+filePath, err)
 	}
 
-	// doing the mapping starts here
-	header := []string{} // to hold the first row of the csv
-
-	for eachLine, record := range rawData {
-		// build the first row and use it as keys
-		if eachLine == 0 {
-			for i := 0; i < len(record); i++ {
-				header = append(header, strings.TrimSpace(record[i]))
-			}
-		} else {
-			// add the values based on the keys from first line
-			line := map[string]int{}
-			for i := 0; i < len(record); i++ {
-				// convert the values from string to int
-				v := record[i]
-				if s, err := strconv.Atoi(v); err == nil {
-					line[header[i]] = s
-				}
-			}
-			returnMap = append(returnMap, line)
-		}
-	}
-	return returnMap
+	return rawData
 }
 
-func main() {
-	checkInput()
-	inputPath := os.Args[1]
-	outputPath := os.Args[2]
-	csvRecords := readCsvFile(inputPath)
+// write the a json to each line of the output document
+func writeData(newData [][]string, filePath string) (returnString string) {
 
-	// create the json for the records
-	jsonOut, err := json.Marshal(csvRecords)
+	//declaring variables
+	var csvHead []string //setting this up to create the head from first column
+
+	// create and open a new document to hold the data
+	f, err := os.OpenFile(filePath, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	//writing the json output
-	err_new := os.WriteFile(outputPath, jsonOut, 0664)
-	if err_new != nil {
+	// iterating through the data to write each line
+	for eachLine, record := range newData {
+		// build the first row
+		if eachLine == 0 {
+			csvHead = buildHead(record)
+
+		} else {
+			// create a map of the first string and all subsequent lines
+			line := createMap(csvHead, record)
+
+			// create the json for the records
+			jsonOut, err := json.Marshal(line)
+			if err != nil {
+				log.Fatal(err)
+			}
+
+			// write the json file to a new line in the document
+			if _, err := f.Write(jsonOut); err != nil {
+				log.Fatal(err)
+			}
+			// write a /n to the document
+			if _, err := f.Write([]byte("\n")); err != nil {
+				log.Fatal(err)
+			}
+		}
+	}
+
+	// close the new document that was created
+	if err := f.Close(); err != nil {
 		log.Fatal(err)
 	}
+
+	returnString = "Your file is ready"
+
+	return returnString
+}
+
+// builds the header of a csv file
+func buildHead(inputSlice []string) []string {
+
+	header := []string{}
+
+	for i := 0; i < len(inputSlice); i++ {
+		header = append(header, strings.TrimSpace(inputSlice[i]))
+	}
+	return header
+}
+
+// create a map
+func createMap(header []string, record []string) map[string]int {
+
+	line := map[string]int{}
+
+	for i := 0; i < len(record); i++ {
+		// convert the values from string to int
+		v := record[i]
+		if s, err := strconv.Atoi(v); err == nil {
+			line[header[i]] = s
+		}
+	}
+	return line
+}
+
+func main() {
+
+	var inputPath, outputPath string = "housesInput.csv", "housesOutput.jsonl"
+	if len(os.Args) == 2 {
+		inputPath = os.Args[2]
+	} else if len(os.Args) > 2 {
+		inputPath = os.Args[2]
+		outputPath = os.Args[3]
+	}
+
+	csvRecords := createData(inputPath)
+
+	fmt.Println(writeData(csvRecords, outputPath))
 }
